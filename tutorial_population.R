99c54700fbd9d431f20a77cbd130e2779c7cc980
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-06-22 14:45:49 +0530

    population working slider

--- tutorial_population.R
+++ tutorial_population.R
@@ -12,26 +12,28 @@ head(world.cities)
 ui <- fluidPage(
   titlePanel("My first shiny app"),
   leafletOutput("mymap"),
-  fluidRow(column(2, 
-                  sliderInput("slider1"), h3("Select the Population"),
-                  min = 0, max = 1000000, value = 1000000),
-          radioButtons("radio", h3("Select the country"),
-                       choices = list("Poland" = "Poland", "Norway" = "Norway", 
-                                      "Belgium" = "Belgium"), selected = "Poland")
-           )
+  fluidRow(
+    column(2,
+      sliderInput("slider1"), h3("Select the Population"),
+      min = 0, max = 1000000, value = 1000000
+    ),
+    radioButtons("radio", h3("Select the country"),
+      choices = list(
+        "Poland" = "Poland", "Norway" = "Norway",
+        "Belgium" = "Belgium"
+      ), selected = "Poland"
+    )
+  )
 )
 server <- function(input, output, session) {
-  
-  output$mymap <-renderLeaflet({
-    
+  output$mymap <- renderLeaflet({
     leaflet(world.cities %>%
-              dplyr::filter(
-                country.etc == input$radio,
-                pop >input$slider1
-              )) %>%
+      dplyr::filter(
+        country.etc == input$radio,
+        pop > input$slider1
+      )) %>%
       addTiles() %>%
-      addMarkers(lat = ~lat, lng =~ long)
-    
+      addMarkers(lat = ~lat, lng = ~long)
   })
 }
 
