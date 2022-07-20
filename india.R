e8aca10144b1882957facbfd21ff4a6b167b3c1f
AkhileshNegi <akhilesh.negi@coloredcow.in>
2022-07-20 10:57:03 +0530

    r shiny with zila name as popup

--- india.R
+++ india.R
@@ -7,33 +7,35 @@ library(sf)
 
 
 
-IND_2 <- getData("GADM", country = "IND", level = 2)
+IND_2 <- getData("GADM", country = "IND", level = 3)
 pal <- colorFactor(
-    palette = "Paired",
-    domain = IND_2@data$NAME_1
+  palette = "Paired",
+  domain = IND_2@data$NAME_1
 )
 IND_2 %>%
-    leaflet() %>%
-    addProviderTiles(providers$Esri.WorldShadedRelief) %>%
-    addPolygons(
-        weight = 1,
-        stroke = TRUE,
-        color = "white",
-        fillColor = ~ pal(NAME_1),
-        fillOpacity = 0.7,
-        dashArray = "3",
-        label = ~NAME_2,
-        popup = ~ paste(
-            "District:", NAME_2,
-            "<br/>",
-            "State:", NAME_1,
-            "<br/>",
-            "Country:", NAME_0
-        ),
-        highlight = highlightOptions(
-            weight = 2,
-            dashArray = "",
-            color = "grey",
-            bringToFront = TRUE
-        )
+  leaflet() %>%
+  addProviderTiles(providers$Esri.WorldShadedRelief) %>%
+  addPolygons(
+    weight = 1,
+    stroke = TRUE,
+    color = "white",
+    fillColor = ~ pal(NAME_1),
+    fillOpacity = 0.7,
+    dashArray = "3",
+    label = ~NAME_3,
+    popup = ~ paste(
+      "Zila:", NAME_3,
+      "<br/>",
+      "District:", NAME_2,
+      "<br/>",
+      "State:", NAME_1,
+      "<br/>",
+      "Country:", NAME_0
+    ),
+    highlight = highlightOptions(
+      weight = 2,
+      dashArray = "",
+      color = "red",
+      bringToFront = TRUE
     )
+  )
